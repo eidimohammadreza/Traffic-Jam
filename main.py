2016905af82a580bde6fdbea0ed86d20cd4cb96a
Mohammadreza Eidi <eidi.mohammadreza@gmail.com>
2017-03-11 18:11:12 +0330

    Minor chnges

--- main.py
+++ main.py
@@ -3,13 +3,17 @@
 
 import numpy as np
 import matplotlib.pyplot as pl
+import random
+
+#import street
+import Car
 
 
-vi=float(input("please enter the initial velocity="))
-nm=float(input("please enter the max number of cars in the street="))
-dt=float(input("please enter the time step for computation (e.g. 0.1, 0.01, ...)="))
-tt=float(input("please enter the total time of simulation (based on min="))
-ti=float(input("please enter the time interval for adding new car to street= "))
+vi=float(input("please enter the initial velocity in term of km/h = "))
+nm=int(input("please enter the max number of cars in the street = "))
+dt=float(input("please enter the time step for computation (e.g. 0.1, 0.01, ...) = "))
+tt=float(input("please enter the total time of simulation (in term of min) = "))
+ti=float(input("please enter the time interval for adding new car to street (in term of Sec) =  "))
 
 tt*=60
 
@@ -18,12 +22,12 @@ tv=[]
 
 # creation of target of velecities
 
-for i in range of (nm+1):
-    tv=uniform(2*vi,10*vi)
+for i in range (nm+1):
+    tv=random.uniform(2*vi,10*vi)
     
 
-for t in range of (0,tt,dt) :
-    if t % ti == 0 : 
+for t in range (0,tt,dt) :
+    if t % ti == 0:
         st.append(t)
-        
+    
 
