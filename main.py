74fcda2260fd03950056f201050b9b0b5b530ce7
Mohammadreza Eidi <eidi.mohammadreza@gmail.com>
2017-03-11 16:20:52 +0330

    Main part

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-# its a comment
+# main part of Traffic Jam 
+
+
+import numpy as np
+import matplotlib.pyplot as pl
+
+
+vi=input("please enter the initial velocity=")
+nm=input("please enter the max number of cars in the street=")
+dt=input("please enter the time step for computation (e.g. 0.1, 0.01, ...)=")
+tt=input("please enter the total time of simulation (based on min=")
+ti=input("please enter the time interval for adding new car to street= ")
+
+tt*=60
+
+st=[]   # creation of street
+tv=[]
+
+# creation of target of velecities
+
+for i in range of (nm+1):
+    tv=uniform(2*vi,10*vi)
+    
+
+for t in range of (0,tt,dt) :
+    if t % ti == 0 : 
+        st.append(t)
