ea38cff8ea0655bd9f5361789672638208ec042b
Hossein <hiravani1983@gmail.com>
2017-03-11 17:48:22 +0330

    Lets go!

---
+++ Street-Class_Traffic-Jam.py
@@ -0,0 +1,10 @@
+import main.py
+class Street:
+    """A one-dimentional linear case"""
+    Length = float(input("Please enter the street Length: "))
+    name = int(input("Please enter the street name: "))
+    
+    def add_car:
+        add_car
+        for i in range(1, nm):
+        
