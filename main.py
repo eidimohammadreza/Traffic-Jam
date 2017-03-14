5bc0b57b3a06361ffccc2ddbe91995ead13b2fcf
Ramtin Didab <ramtindidab@gmail.com>
2017-03-14 20:07:59 +0330

    moved the generation scope to a function: generate_cars(i)

--- main.py
+++ main.py
@@ -19,6 +19,16 @@ def prob(p):
     else:
         return False
 
+def generate_cars(i):
+    if i%generation_step == 0:
+        #later : check if there's no car in the generating point
+        target_v = 7 #random.uniform(5, 10)
+        x, v, min_dist = 0, random.uniform(1, target_v), random.uniform(1,5)
+        print "car created with:", x, v, min_dist, target_v, "\n"
+        c = Car(x, v, min_dist, target_v)
+        s.add_car(c)
+        lines.append(ax2.plot([], [], '-', lw=1)[0])
+
 # taking inputs from the user:
 # for testing purposes, I've commented them so not being stuck with typing inputs every time we test the main.py
 # hint: a way of getting inputs as argument values should be implemented
@@ -78,19 +88,8 @@ def init():
 
 
 def animate(i):
+    generate_cars(i) #Checks if should generate a new car, and does it
 
-    #Checks if should generate a new car:
-   
-    #print "- step ", i, ":"
-    if i%generation_step == 0:
-        #later : check if there's no car in the generating point
-        target_v = 7 #random.uniform(5, 10)
-        x, v, min_dist = 0, random.uniform(1, target_v), random.uniform(1,5)
-        print "car created with:", x, v, min_dist, target_v, "\n"
-        c = Car(x, v, min_dist, target_v)
-        s.add_car(c)
-        lines.append(ax2.plot([], [], '-', lw=1)[0])
-    
     #sets time and gets the position of each car. y position is set to zero by default.
     t=i*dt
 
@@ -100,7 +99,6 @@ def animate(i):
         if prob(0.1):
             bad_car = s.cars_[ random.randint( 0, len(s.cars) -1 ) ]
     """
-
 
     if len(s.cars)>2 and len(s.cars) < 20:
         bad_car = s.cars_[1]
